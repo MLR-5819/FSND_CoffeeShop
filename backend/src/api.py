e66173393c02b03039abe561542ec612a331c9dc
MLR-5819 <58010528+MLR-5819@users.noreply.github.com>
2020-08-16 18:13:27 -0500

    Update api.py

--- backend/src/api.py
+++ backend/src/api.py
@@ -24,7 +24,8 @@ CORS(app)
 #   GET /drinks
 #       it should be a public endpoint
 #       it should contain only the drink.short() data representation
-#   returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+#           returns status code 200 and json {"success": True, "drinks": drinks} 
+#           where drinks is the list of drinks
 #       or appropriate status code indicating reason for failure
 
 @app.route('/drinks', methods=['GET'])
@@ -45,7 +46,8 @@ def get_drinks():
 #   GET /drinks-detail
 #       it should require the 'get:drinks-detail' permission
 #       it should contain the drink.long() data representation
-#   returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+#           returns status code 200 and json {"success": True, "drinks": drinks} 
+#           where drinks is the list of drinks
 #       or appropriate status code indicating reason for failure
 
 @app.route('/drinks-detail', methods=['GET'])
@@ -68,7 +70,8 @@ def drinks_detail(token):
 #       it should create a new row in the drinks table
 #       it should require the 'post:drinks' permission
 #       it should contain the drink.long() data representation
-#   returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
+#           returns status code 200 and json {"success": True, "drinks": drink} 
+#           where drink an array containing only the newly created drink
 #       or appropriate status code indicating reason for failure
 
 @app.route('/drinks', methods=['POST'])
@@ -101,7 +104,8 @@ def create_drink(token):
 #       it should update the corresponding row for <id>
 #       it should require the 'patch:drinks' permission
 #       it should contain the drink.long() data representation
-#   returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
+#           returns status code 200 and json {"success": True, "drinks": drink} 
+#           where drink an array containing only the updated drink
 #       or appropriate status code indicating reason for failure
 
 @app.route('/drinks/<int:id>', methods=['PATCH'])
@@ -135,7 +139,8 @@ def update_drink(token, id):
 #       it should respond with a 404 error if <id> is not found
 #       it should delete the corresponding row for <id>
 #       it should require the 'delete:drinks' permission
-#   returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
+#           returns status code 200 and json {"success": True, "delete": id} 
+#           where id is the id of the deleted record
 #       or appropriate status code indicating reason for failure
 
 @app.route('/drinks/<int:id>', methods=['DELETE'])
