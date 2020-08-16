9f7c0073667a9b106d86c364e12b2ee475eb5584
MLR-5819 <58010528+MLR-5819@users.noreply.github.com>
2020-08-16 18:13:34 -0500

    Update auth.py

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -87,7 +87,8 @@ def check_permissions(permission, payload):
 # it should decode the payload from the token
 # it should validate the claims
 # return the decoded payload
-# !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
+# !!NOTE urlopen has a common certificate error described here: 
+# https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
 # source: udacity FSND lesson
 
 def verify_decode_jwt(token):
